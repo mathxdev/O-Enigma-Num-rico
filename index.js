d41e74523567663351d99f0592364cbc0f00982c
jenixcri <jclc2@cesar.school>
2024-11-17 17:50:02 -0300

    fiz o básico do js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-numeroSecreto = parseInt(Math.random()*10)
+let numeroSecreto = parseInt(Math.random()*10);
+let texto = document.getElementById('texto');
+
+function verificarChute(){
+    let chute = document.querySelector('input').value;
+    while (true) {
+   
+        if (numeroSecreto < chute){
+            texto.innerText = 'O número secreto é menor!'
+            return
+        }
+        if (numeroSecreto > chute){
+            texto.innerText = 'O número secreto é maior!'
+            return
+        }
+        if (numeroSecreto == chute){
+            texto.innerText = 'Você acertou!'
+            break
+        }
+    }
+
+}
+
